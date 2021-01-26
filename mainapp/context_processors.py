605a8b6e9f755c5c15eeea88c1d5f0fb90d53e2b
Valerii Sh <vshaliagin@mail.ru>
2021-01-26 23:24:25 +1000

    les-6_2

--- mainapp/context_processors.py
+++ mainapp/context_processors.py
@@ -4,7 +4,8 @@ from basketapp.models import Basket
 def basket(request):
     basket_item = []
     if request.user.is_authenticated:
-        basket_item = Basket.objects.filter(user=request.user)
+        # basket_item = Basket.objects.filter(user=request.user)
+        basket_item = Basket.objects.filter(user=request.user).order_by('product__category').select_related()
     return {
         'basket': basket_item
     }
